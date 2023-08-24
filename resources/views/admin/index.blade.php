49b8e58b0a03047cd11e9c3258ad8376df6f8074
Francis Mutura <frankmutura@gmail.com>
2023-08-24 14:44:05 +0300

    Initial commit

---
+++ resources/views/admin/index.blade.php
@@ -0,0 +1,10 @@
+@extends('layouts.admin')
+
+@section('content')
+<div class="card">
+    <div class="card-body color-text">
+        <h2>Welcome to H&S Engineering Services </h2>
+        <h3>Your Trusted Electrical & Telecommunications Services Partner</h3>
+    </div>
+</div>
+@endsection
